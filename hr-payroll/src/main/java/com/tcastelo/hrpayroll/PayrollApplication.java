f8c22cb1a29d74a33b6ce998df562af7315acee5
tcastelo <thiago231295@gmail.com>
2021-06-24 23:59:08 -0300

    iniciando projeto eureka server

--- hr-payroll/src/main/java/com/tcastelo/hrpayroll/PayrollApplication.java
+++ hr-payroll/src/main/java/com/tcastelo/hrpayroll/PayrollApplication.java
@@ -3,11 +3,13 @@ package com.tcastelo.hrpayroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableFeignClients
+@RibbonClient(name = "hr-worker")
 public class PayrollApplication {
 
     public static void main(String[] args) {
--- hr-payroll/src/main/java/com/tcastelo/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/tcastelo/hrpayroll/feignclients/WorkerFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8080", path = "/api/workers")
+@FeignClient(name = "hr-worker", path = "/api/workers")
 public interface WorkerFeignClient {
 
     @GetMapping("{id}")
