2c21ff8e87c1fecf4e2dc3aa35d53ffca8bf1dff
tcastelo <thiago231295@gmail.com>
2021-06-26 16:22:16 -0300

    configurando config server e actuator

--- hr-worker/src/main/java/com/tcastelo/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/tcastelo/hrworker/controller/WorkerController.java
@@ -3,6 +3,8 @@ package com.tcastelo.hrworker.controller;
 import com.tcastelo.hrworker.entity.Worker;
 import com.tcastelo.hrworker.repository.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/workers")
+@RefreshScope
 public class WorkerController {
 
     @Autowired
@@ -26,5 +29,13 @@ public class WorkerController {
     @GetMapping("{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
         return ResponseEntity.ok(repository.findById(id).get());
+    }
+
+    @Value("${test.config}")
+    private String testConfig;
+
+    @GetMapping("/config")
+    public ResponseEntity<String> getConfig(){
+        return ResponseEntity.ok(testConfig);
     }
 }
